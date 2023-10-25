6ab4fbb9a662395b92cb151898983af363431086
Rosalia Russo <rosy.russo1.99@gmail.com>
2023-10-25 14:07:14 +0200

    aggiunto campi in dettaglio

--- src/app/add-film/add-film.component.ts
+++ src/app/add-film/add-film.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FilmService } from '../film.service';
 import { Film } from '../model/Film';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-film',
@@ -9,14 +10,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-film.component.css']
 })
 export class AddFilmComponent {
+  imageURL: string = '';
   title:string = "";
   description:string = "";
-  constructor(private filmService: FilmService,private router: Router) {}
+  actors!: string;
+  duration!: string;
+  year!: string;
+  price!: number;
+  category!: string;
+  categories: { name: string }[] = [];
+
+  constructor(private filmService: FilmService,private router: Router, private http: HttpClient) {
+    this.getCategories();
+  }
 
   addFilmHandler() {
-    const nuovoFilm: Film = new Film(this.title,this.description); // Crea un nuovo film
-    this.filmService.addFilm(nuovoFilm); // Aggiungi il film utilizzando il servizio
+    const nuovoFilm: Film = new Film(this.title,this.description,this.imageURL, this.actors, this.duration, this.year, this.price, this.category); 
+    this.filmService.addFilm(nuovoFilm); 
     this.router.navigate(["/catalogue"])
 
   }
+
+  getCategories() {
+    this.http.get<any>('http://localhost:8080/api/v1/categories').subscribe(data => {
+      
+      this.categories = data; 
+    });
+  }
+  
 }
--- src/app/dettaglio-film/dettaglio-film.component.ts
+++ src/app/dettaglio-film/dettaglio-film.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilmService } from '../film.service';
+import { HttpClient } from '@angular/common/http';
+import { Film } from '../model/Film';
 
 interface Movie {
   title: string;
@@ -10,44 +13,22 @@ interface Movie {
   templateUrl: './dettaglio-film.component.html',
   styleUrls: ['./dettaglio-film.component.css']
 })
-export class DettaglioFilmComponent {
-  movies: Movie[] = [
-    {
-      title: 'Inception',
-      description: 'Un ladro esperto di estrarre informazioni segrete ruba segreti corporate inserendosi nei sogni delle persone.'
-    },
-    {
-      title: 'The Shawshank Redemption',
-      description: 'La storia di Andy Dufresne, un banchiere che è stato condannato all\'ergastolo per l\'omicidio di sua moglie e del suo amante.'
-    },
-    {
-      title: 'The Godfather',
-      description: 'La storia della famiglia Corleone, con particolare attenzione sul figlio più giovane, Michael, che prende la guida della famiglia.'
-    },
-    {
-      title: 'The Dark Knight',
-      description: 'Il Cavaliere Oscuro si trova a combattere una minaccia mortale proveniente da Gotham City, rappresentata dal Joker.'
-    },
-    {
-      title: 'Schindler\'s List',
-      description: 'La storia di Oskar Schindler, un uomo d\'affari tedesco che ha salvato la vita a molti ebrei polacchi durante l\'Olocausto.'
-    },
-    {
-      title: 'Pulp Fiction',
-      description: 'Una serie di storie intrecciate che ruotano attorno a due criminali, una moglie del boss e un pugile incallito.'
-    },
-    {
-      title: 'The Lord of the Rings: The Return of the King',
-      description: 'Frodo e Sam si avvicinano al Monte Fato per distruggere l\'Unico Anello, mentre l\'esercito di Aragorn si prepara per la battaglia.'
-    }
-    
-  ];
+export class DettaglioFilmComponent implements OnInit {
+  
+ 
+ 
 
-  selectedMovie: Movie | undefined; 
+  selectedMovie: Film | undefined; 
 
-  constructor() {
+  constructor(private filmService: FilmService, private http: HttpClient) {
    
-    this.selectedMovie = this.movies[0];
+    this.selectedMovie = this.filmService.getFilms()[0]; 
+
   }
+  ngOnInit(): void {
+     
+  }
+  
+  
 
 }
